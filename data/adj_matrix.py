850a8271caaf66352c8a8cb3a5989071be53f9d3
sycamoretr <151596872+sycamoretr@users.noreply.github.com>
2023-11-29 15:13:44 +0800

    Update adj_matrix.py

--- data/adj_matrix.py
+++ data/adj_matrix.py
@@ -23,9 +23,21 @@ def distance_matrix(prot_id, seq, c_coords):
 
 
 if __name__ == '__main__':
-    file_path = 'traindata.fasta'
-    coordinate_dict = np.load('train_coordinate_dict.npy',allow_pickle=True).item()
-    for index, record in tqdm(enumerate(SeqIO.parse(file_path, 'fasta'))):
-        c_coords = coordinate_dict[str(record.id)]
+    file_path1 = 'train_data.fasta'
+    coordinate_dict1 = np.load('train_coordinate_dict.npy',allow_pickle=True).item()
+    for index, record in tqdm(enumerate(SeqIO.parse(file_path1, 'fasta'))):
+        c_coords = coordinate_dict1[str(record.id)]
+        distance_matrix(str(record.id), str(record.seq),c_coords)
+        
+    file_path2 = 'val_data.fasta'
+    coordinate_dict2 = np.load('val_coordinate_dict.npy',allow_pickle=True).item()
+    for index, record in tqdm(enumerate(SeqIO.parse(file_path2, 'fasta'))):
+        c_coords = coordinate_dict2[str(record.id)]
+        distance_matrix(str(record.id), str(record.seq),c_coords)
+        
+    file_path3 = 'testdata.fasta'
+    coordinate_dict3 = np.load('test_coordinate_dict.npy',allow_pickle=True).item()
+    for index, record in tqdm(enumerate(SeqIO.parse(file_path3, 'fasta'))):
+        c_coords = coordinate_dict3[str(record.id)]
         distance_matrix(str(record.id), str(record.seq),c_coords)
 
