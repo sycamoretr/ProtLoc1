8d96afb4b8dd27cc5e6b629868d2509f20a03b33
sycamoretr <151596872+sycamoretr@users.noreply.github.com>
2023-11-29 14:51:07 +0800

    Update adj_matrix.py

--- data/adj_matrix.py
+++ data/adj_matrix.py
@@ -1,1 +1,31 @@
+import numpy as np
+import csv
+from Bio import SeqIO
+from tqdm import tqdm
+from Bio import PDB
+import warnings
+
+
+
+
+def distance_matrix(prot_id, seq, c_coords):
+    c_coords = np.array(c_coords)
+    dist_matrix = np.zeros((len(seq), len(seq)))
+    for i, coord1 in enumerate(c_coords):
+        for j, coord2 in enumerate(c_coords):
+            dist = np.sqrt(np.sum((coord1 - coord2) ** 2))
+            dist_matrix[i, j] = dist
+
+    with open('./adj_matrix/' + prot_id + '.csv', mode='w', newline='') as file:
+        writer = csv.writer(file, delimiter=',')
+        for row in dist_matrix:
+            writer.writerow(row)
+
+
+if __name__ == '__main__':
+    file_path = 'traindata.fasta'
+    coordinate_dict = np.load('train_coordinate_dict.npy',allow_pickle=True).item()
+    for index, record in tqdm(enumerate(SeqIO.parse(file_path, 'fasta'))):
+        c_coords = coordinate_dict[str(record.id)]
+        distance_matrix(str(record.id), str(record.seq),c_coords)
 
